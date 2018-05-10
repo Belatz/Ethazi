fefc53e7587b94456f13c82fd66fdefc39b189d8
Belatz <35420854+Belatz@users.noreply.github.com>
2018-05-10 10:01:18 +0200

    Add files via upload

--- Busqueda de Empleo/src/ethazi/intefaz/VentanaPrincipal.java
+++ Busqueda de Empleo/src/ethazi/intefaz/VentanaPrincipal.java
@@ -6,6 +6,11 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JInternalFrame;
+import javax.swing.JLayeredPane;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class VentanaPrincipal extends JFrame {
 
@@ -28,15 +33,32 @@ public class VentanaPrincipal extends JFrame {
 	}
 
 	/**
-	 * Create the frame.
+	 * Create the frame
 	 */
 	public VentanaPrincipal() {
+		setResizable(false);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 744, 517);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JPanel panel_barraHerramientas = new JPanel();
+		panel_barraHerramientas.setBounds(5, 5, 728, 50);
+		contentPane.add(panel_barraHerramientas);
+		panel_barraHerramientas.setLayout(null);
+		
+		JButton btnNewButton = new JButton("");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+			}
+		});
+		btnNewButton.setBounds(0, 0, 50, 50);
+		panel_barraHerramientas.add(btnNewButton);
+		
+		JLayeredPane layeredPane = new JLayeredPane();
+		layeredPane.setBounds(5, 56, 728, 422);
+		contentPane.add(layeredPane);
 	}
-
 }
