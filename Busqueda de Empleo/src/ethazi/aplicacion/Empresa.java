2e9b96a3a5d0d0502bffa42f64157fceb20efbaa
mañana <maana@ORD38>
2018-05-14 14:25:01 +0200

    Usuario acabado sin metodos codificados Enumeracion acabada Empresa amedias

--- Busqueda de Empleo/src/ethazi/aplicacion/Empresa.java
+++ Busqueda de Empleo/src/ethazi/aplicacion/Empresa.java
@@ -1,5 +1,33 @@
 package ethazi.aplicacion;
 
+import java.util.ArrayList;
+
 public class Empresa extends Usuario {
-	// Prueba
+	
+	private String miContacto;
+	private String miDescripcion;
+	private ArrayList<Oferta> miOfertas;
+	public Empresa(String miNick, String miPassword, String miNombre, String miNumID, String miDireccion,
+			String miEmail, String miTelefono, byte miAniosExp, String miContacto, String miDescripcion,
+			ArrayList<Oferta> miOfertas) throws NullPointerException {
+		super(miNick, miPassword, miNombre, miNumID, miDireccion, miEmail, miTelefono, miAniosExp);
+		this.miContacto = miContacto;
+		this.miDescripcion = miDescripcion;
+		this.miOfertas = miOfertas;
+	}
+	
+	@Override
+	public void editarPerfil() {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void verPerfil() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
 }
+
+
