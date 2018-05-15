fe7e6191aabdcafeb974c246fb70dd419cd56dc8
Belatz <belatz.arcega@elorrieta-errekamari.com>
2018-05-15 21:26:57 +0200

    Conexion y metodos de BD

--- Busqueda de Empleo/src/ethazi/bd/Conexion.java
+++ Busqueda de Empleo/src/ethazi/bd/Conexion.java
@@ -2,6 +2,9 @@ package ethazi.bd;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class Conexion {
 
@@ -46,6 +49,36 @@ public class Conexion {
 		if (miConexion == null) {
 			System.out.println("Conexion con la base de datos terminada..");
 		}
+	}
+
+	/**
+	 * Metodo que vale para hacer actualizaciones (Insert, Delete o Update) a la
+	 * base de datos
+	 * 
+	 * @param sql
+	 *            Recibe como parametro la instruccion SQL en forma de String
+	 * @return Devuelve (1) si la actualizacion ha afectado a alguna fila, o (0) si
+	 *         no ha afectado a ninguna fila
+	 * @throws SQLException
+	 *             Si ha habido algun error en la base de datos o en la consulta
+	 */
+	public int actualizar(String sql) throws SQLException {
+		Statement _comando = miConexion.createStatement();
+		return _comando.executeUpdate(sql);
+	}
+
+	/**
+	 * Metodo que vale para hacer consultas (Select) a la base de datos
+	 * 
+	 * @param sql
+	 *            Recibe como parametro la instruccion SQL en forma de String
+	 * @return Devuelve el resultado de la consulta en forma de ResultSet
+	 * @throws SQLException
+	 *             Si ha habido algun error en la base de datos o en la consulta
+	 */
+	public ResultSet consultar(String sql) throws SQLException {
+		Statement _comando = miConexion.createStatement();
+		return _comando.executeQuery(sql);
 	}
 
 }
