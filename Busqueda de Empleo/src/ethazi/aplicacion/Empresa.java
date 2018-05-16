92167299df02adcef0601c735b24df10e3f35d12
mañana <maana@ORD38>
2018-05-16 10:18:47 +0200

    USUARIO Y EMPRESA TERMINADOS CANDIDATO EN CURSO

--- Busqueda de Empleo/src/ethazi/aplicacion/Empresa.java
+++ Busqueda de Empleo/src/ethazi/aplicacion/Empresa.java
@@ -1,12 +1,14 @@
 package ethazi.aplicacion;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 
 public class Empresa extends Usuario {
-	
+
 	private String miContacto;
 	private String miDescripcion;
 	private ArrayList<Oferta> miOfertas;
+
 	public Empresa(String miNick, String miPassword, String miNombre, String miNumID, String miDireccion,
 			String miEmail, String miTelefono, byte miAniosExp, String miContacto, String miDescripcion,
 			ArrayList<Oferta> miOfertas) throws NullPointerException {
@@ -15,19 +17,46 @@ public class Empresa extends Usuario {
 		this.miDescripcion = miDescripcion;
 		this.miOfertas = miOfertas;
 	}
+
+	public void publicarOferta(Oferta p_Oferta) {
+
+	}
+
+	public void crearConocimiento(String p_Conocimiento) {
+
+	}
+
+	public ArrayList<Oferta> consultarMisOfertas() {
+		ArrayList<Oferta> _ofertas = null;
+		return _ofertas;
+	}
+
+	public ArrayList<Candidato> buscarCandidatos(Calendar _FechaDeNacimiento, boolean _Coche, boolean _CarnetConducir,
+			boolean _DisponibilidadParaViajar, ArrayList<String> _Conocimientos, String _OtrosConocimientos, byte _ExperienciaLaboral, String _VidaLaboral) {
+		ArrayList<Candidato> _candidatos = null;
+		return _candidatos;
+	}
 	
+	public ArrayList<Oferta> verOfertasConSolicitud(){
+		ArrayList<Oferta> _ofertas = null;
+		return _ofertas;
+		
+	}
+	
+	public void guardarCurriculum(Candidato _perfil) {
+		
+	}
+
 	@Override
 	public void editarPerfil() {
 		// TODO Auto-generated method stub
-		
+
 	}
+
 	@Override
 	public void verPerfil() {
 		// TODO Auto-generated method stub
-		
+
 	}
-	
-	
+
 }
-
-
