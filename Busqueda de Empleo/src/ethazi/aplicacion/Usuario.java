438e1a81d023dda6f8b726fda130b2f74958e63e
Belatz <belatz.arcega@elorrieta-errekamari.com>
2018-05-18 19:19:53 +0200

    Utilidades

--- Busqueda de Empleo/src/ethazi/aplicacion/Usuario.java
+++ Busqueda de Empleo/src/ethazi/aplicacion/Usuario.java
@@ -4,7 +4,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 
-public class Usuario {
+public abstract class Usuario {
 	public static ArrayList<String> misConocimientosTotales;
 
 	private String miNumID;
